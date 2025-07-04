aa7fe9e4ddb0c7f63909c7a36270f823e6b135b9
xs <1960654075@qq.com>
2025-07-04 17:55:50 +0800

    no message

--- index.d.ts
+++ index.d.ts
@@ -1,6 +1,6 @@
 declare module "image-slider-scale" {
     /**
-     * 插入 HTML 到指定 DOM 元素
+     * 图片对比
      * @param {string} domId - 目标 DOM 元素的 ID
      * @param {string} originalImg - 原图(顶图)
      * @param {string} generateImg - 生成图(底图)
